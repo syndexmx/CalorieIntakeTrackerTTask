5dc779c3a5354c43db6492f0a9d08ba6c596742e
Maxim Shapovalov <syndex@list.ru>
2025-03-24 19:51:57 +0300

    Initial commit

---
+++ src/test/java/com/github/syndexmx/caloryintaketracker/CaloryintaketrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.syndexmx.caloryintaketracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaloryintaketrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
