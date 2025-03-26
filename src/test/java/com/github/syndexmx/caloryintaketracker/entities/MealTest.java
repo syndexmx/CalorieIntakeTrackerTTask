3aa167b3832a2694d572d819b18ecd3ff28b5af2
Maxim Shapovalov <syndex@list.ru>
2025-03-26 19:37:57 +0300

    Meal Calorie content Test implemented

---
+++ src/test/java/com/github/syndexmx/caloryintaketracker/entities/MealTest.java
@@ -0,0 +1,25 @@
+package com.github.syndexmx.caloryintaketracker.entities;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MealTest {
+
+    @Test
+    void getCalories() {
+        Dish dish = TestDishes.getTestDish();
+        Dish dish2 = TestDishes.getTestDish(2);
+        Meal meal = Meal.builder()
+                .id(0L)
+                .date(LocalDate.parse("2000-01-01"))
+                .dishList(List.of(dish, dish2))
+                .build();
+        int calculatedCalories = meal.getCalories();
+        int expected = 1100;
+        assertEquals(expected ,calculatedCalories);
+    }
+}
